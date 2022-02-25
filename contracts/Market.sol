a9e942a20af2d8cace0ce79247cecc58a5fc6d90
SlokAks <slok.aks204@gmail.com>
2022-02-26 00:19:39 +0530

    implemented listToken()

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -3,7 +3,25 @@ pragma solidity ^0.8.9;
 
 contract Market {
 
-    function listToken() public {
-        
+    struct Listing {
+        address token;
+        uint tokinId;
+        uint price;
     }
+
+    uint private _listingId = 0;
+    mapping (uint => Listing) private _listings;
+
+    function listToken(address token, uint tokenId, uint price) public {
+        Listing memory listing = Listing(
+            token, 
+            tokenId,
+            price
+        );
+
+        _listingId++;
+        _listings[_listingId] = listing;
+    }
+
+    
 }
